ce3d6eb0a4151d6bf744f95c401fd0885f01168f
alexeyb7 <alexeyb7@gmail.com>
2023-10-10 00:43:23 +0300

    Modified App.java

--- dz3/src/App.java
+++ dz3/src/App.java
@@ -1,11 +1,15 @@
 import java.io.FileWriter;
 import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите данные в следующем порядке: Фамилия Имя Отчество ДатаРождения НомерТелефона Пол");
+        System.out.println(
+                "Введите данные в следующем порядке: Фамилия Имя Отчество ДатаРождения:dd-MM-yyyy НомерТелефона Пол: f или m");
 
         try {
             String input = scanner.nextLine();
@@ -16,9 +20,31 @@ public class App {
             String firstName = userData[1];
             String middleName = userData[2];
             String dateOfBirth = userData[3];
+            SimpleDateFormat ft = new SimpleDateFormat("dd-MM-yyyy");
+            Date parsingDate;
+            try {
+                parsingDate = ft.parse(dateOfBirth);
+                System.out.println(parsingDate);
+            } catch (ParseException e) {
+                System.out.println("Неправильно введена дата");
+            }
             String phoneNumber = userData[4];
+            try {
+                int phoneNumb = Integer.parseInt(phoneNumber);
+            } catch (NumberFormatException e) {
+                System.out.println("Неправильный ввод телефонного номера");
+            }
             String gender = userData[5];
-
+            switch (gender) {
+                case "f":
+                    System.out.println(gender);
+                    break;
+                case "m":
+                    System.out.println(gender);
+                    break;
+                default:
+                    System.out.println("Неправильно введен пол " + gender);
+            }
             String fileName = lastName + ".txt";
 
             FileWriter fileWriter = new FileWriter(fileName, true);
